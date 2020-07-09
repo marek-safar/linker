6614d5d5fe709175ade73286119399ccc66d155a
Marek Safar <marek.safar@gmail.com>
2020-07-09 20:11:18 +0200

    Adds manual linker attributes removed for assembly and module level attributes

---
+++ test/Mono.Linker.Tests.Cases/DataFlow/Dependencies/AssemblyLevelLinkerAttributeRemoval_Lib.cs
@@ -0,0 +1,19 @@
+using System;
+using Mono.Linker.Tests.Cases.TestAttributeLib;
+
+[assembly: MyAttribute]
+
+namespace Mono.Linker.Tests.Cases.TestAttributeLib
+{
+	[System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+	public sealed class MyAttribute : System.Attribute
+	{
+		public MyAttribute()
+		{
+		}
+	}
+
+	public class Foo
+	{
+	}
+}
